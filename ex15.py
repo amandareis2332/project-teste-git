301c8351217ed6ed9ce3ec936f3a01150818f395
Amanda Reis <amandareis2332@gmail.com>
2022-08-30 17:06:52 -0300

    ex14 e 15

---
+++ ex15.py
@@ -0,0 +1,7 @@
+print("Calculo divisão conta de energia")
+
+quantidade_de_pessoas = input("Quantas pessoas vão dividir a conta?")
+valorconta = input("Qual o valor da conta?")
+divisao = float(valorconta)/int(quantidade_de_pessoas)
+print(divisao)
+
