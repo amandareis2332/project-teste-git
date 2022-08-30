8533ee34f2c2fe8d2741ab785282117a72e83839
Amanda Reis <amandareis2332@gmail.com>
2022-08-30 16:11:52 -0300

    ex 11, 12 e 13

---
+++ ex11.py
@@ -0,0 +1,6 @@
+print("Calculo área quadrado")
+lado = input("Informe qual o tamanho em centimetros do lado")
+areaquadrado = int(lado) * int(lado)
+print("A área do quadrado é: ", areaquadrado, "centimetros quadrados")
+
+
---
+++ ex13.py
@@ -0,0 +1,7 @@
+print("calculo média idades")
+idade1 = int(input('Informe a primeira idade: '))
+idade2 = int(input('Informe a segunda idade: '))
+idade3 = int(input('Informe a terceira idade: '))
+media = (idade1+ idade2 + idade3)/3
+print(media)
+
