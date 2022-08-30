8ff217e9677072c342d2f291604e9b44525b1ebb
Amanda Reis <amandareis2332@gmail.com>
2022-08-30 15:50:07 -0300

    ex11 lista1

--- lista03_02.py
+++ lista03_02.py
@@ -3,9 +3,9 @@
 nome = input("Qual seu nome?")
 cidade = input ("Qual sua cidade natal?")
 ano_de_nascimento = input("Em que ano voce nasceu?")
-print(nome)
-print(cidade)
-print(ano_de_nascimento)
+print("Seu nome: ", nome)
+print("Você nasceu em : ", cidade)
+print("O ano que você nasceu foi: ", ano_de_nascimento)
 idadeem2030 = int(2030) - int(ano_de_nascimento)
 print("No ano de 2030, você terá: ", idadeem2030)
 
